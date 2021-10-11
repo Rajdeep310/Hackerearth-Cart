4d85098dbd4c9f599464c01252e7f592624ea0fd
Rajdeep Dhar <rajdeepdhar1@gmail.com>
2021-10-11 15:49:23 +0530

    TotalPriceContainer Component completed which will show all prices and discounts

--- src/TotalPriceContainer.js
+++ src/TotalPriceContainer.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './Context';
+
+import './TotalPriceContainer.css'
+import './App.css'
 
 function TotalPriceContainer() {
+
+    const {newCart} = useContext(AppContext);
     return (
-        <div>
-            Total Price Container
+        <div className='totalPriceContainer'>
+            <div className='gap'></div>
+            <div className="bord">
+                <h4 className='line'>Total</h4>
+                <div className="line">
+                    <div>Items ({newCart.length})</div> : <div>$ </div>
+                </div>
+                <div className="line">
+                   <div> Discount</div> : <div>$ </div>
+                </div>
+                <div className=" line total">
+                 <div> <h3>Order Total</h3>   </div>  :   <h3>$ </h3> 
+                </div>
+
+            </div>
+
         </div>
     )
 }
